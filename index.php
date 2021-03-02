6f58ad796458f124267ca5f05f56c7e64641c30d
kote2 <kote2kote2@gmail.com>
2021-03-02 11:00:12 +0900

    starter1.0.1

--- index.php
+++ index.php
@@ -17,7 +17,27 @@ get_header();
 ?>
 <main class="main flex w-full">
 　　<div class="inner px-8 w-full h-full cm">
-    メイン
+      <h2 class="c-tail mb-8">メイン</h2>
+        <div class="">
+          text
+        </div>
+        <h3 class="c-tail with-margin">オプション</h3>
+        <div class="">
+          test
+        </div>
+        <h4 class="c-tail with-margin">h4.c-tail</h4>
+        <div class="">
+          test
+        </div>
+        <h5 class="c-tail with-margin">h5.c-tail</h5>
+        <div class="">
+          test
+        </div>
+        <h6 class="c-tail with-margin">h6.c-tail</h6>
+        <div class="">
+          test
+        </div>
+      </div>
 		</div>
 </main>
 <?php
